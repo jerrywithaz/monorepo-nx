c1fee919f5a4a7683da4c581edf4a8ebaa2821e4
jerrywithaz <dev@jerrywithaz.com>
2023-04-21 12:06:49 -0500

    New: Add sayGoodbye

--- packages/package-2/src/lib/package-2.ts
+++ packages/package-2/src/lib/package-2.ts
@@ -3,3 +3,5 @@ import { isEven } from '@zerry/package-1';
 export const isOdd = (x: number) => !isEven(x);
 
 export const sayHello = () => console.log("hello");
+
+export const sayGoodbye = () => console.log("goodbye");
